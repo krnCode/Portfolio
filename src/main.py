035b35bae471482a4150fd3f9e69fc75517d1ed3
krn <contadorkrn@gmail.com>
2025-10-15 15:18:14 -0300

    DEV: Implementado o código no arquivo inicial que será usado para iniciar o app streamlit - contém as páginas do app.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from portfolio!")
+"""
+Arquivo principal do projeto.
+Será usado para controlar as páginas a serem exibidas no Streamlit.
+"""
 
+import streamlit as st
 
-if __name__ == "__main__":
-    main()
+app_pages: dict[str, list[st.Page]] = {
+    "Início": [
+        st.Page("pagina_inicial.py", title="Página Inicial"),
+        st.Page("sobre.py", title="Sobre"),
+    ],
+    "Portifolio": [
+        st.Page("projects.py", title="Meus Projetos"),
+    ],
+}
+
+pg: st.Page = st.navigation(app_pages)
